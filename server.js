cfb44b55357c2ec623c9272c912a275826bda67f
NxHeng <e-heng@live.com>
2024-05-26 16:08:11 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const createRoutes = require('./routes/create');
+
+//express app
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+//routes
+app.use('/create', createRoutes);
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then((result) => {
+        console.log('Connected to db');
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Server is running on port 5000');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
