bb215668c55bb0789dafe57e75b76226151070b4
NxHeng <e-heng@live.com>
2025-01-04 20:28:21 +0800

    fix: api status code for notification

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -6,7 +6,7 @@ const getNotificationsForUser = async (req, res) => {
         const { _id } = req.user; // Assume you have user ID from authentication middleware
         // const notifications = await Notification.find({ usersNotified: userId }).sort({ createdAt: -1 });
         const notifications = await notificationService.getNotificationsForUser(_id);
-        res.json(notifications);
+        res.status(200).json(notifications);
     } catch (error) {
         res.status(500).json({ error: 'Failed to retrieve notifications' });
     }
@@ -16,7 +16,7 @@ const deleteSingleNotification = async (req, res) => {
     try {
         const { notificationId } = req.params;
         await notificationService.deleteSingleNotification(notificationId);
-        res.json({ message: 'Notification deleted' });
+        res.status(200).json({ message: 'Notification deleted' });
     } catch (error) {
         res.status(500).json({ error: 'Failed to delete notification' });
     }
@@ -26,7 +26,7 @@ const deleteAllNotificationsForUser = async (req, res) => {
     try {
         const { _id } = req.user;
         await notificationService.deleteAllNotificationsForUser(_id);
-        res.json({ message: 'All notifications deleted' });
+        res.status(200).json({ message: 'All notifications deleted' });
     } catch (error) {
         res.status(500).json({ error: 'Failed to delete notifications' });
     }
